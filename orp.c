75a16a1d2eccc74bde3b37882054fbc5468a8cd7
ajit0312 <30343853+ajit0312@users.noreply.github.com>
2017-07-21 18:25:11 +0530

    Create orp.c

---
+++ orp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+int n;
+
+printf("enter the n value");
+scanf("%d",&n);
+if(n%2==0)
+printf ("the value is even");
+else
+printf("the number is positive");
+return 0;
+}
