beb20d752a7f0802b1e3ea93399bec534ecbdbf6
ajit0312 <30343853+ajit0312@users.noreply.github.com>
2017-07-24 10:07:11 +0530

    Update p.c

--- p.c
+++ p.c
@@ -4,11 +4,19 @@ int main()
 int base,power,result=1;
 scanf("%d",&base);
 scanf("%d",&power);
+  if(power==0)
+  {
+    result=1;
+    printf("%d",result);
+  }
+  if(power>0)
+  {
 while(power!=0)
 {
 result *=base;
 --power;
 }
 printf("%d",result);
+  }
 return 0;
 }
